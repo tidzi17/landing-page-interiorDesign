c3cd0470a92eb8b7084f5ca7e35370e3a16ad529
tidzi17 <tidzi15110@gmail.com>
2024-03-05 21:17:33 +0100

    responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import img from '../assets/redesign.png';
+import redesign from '../assets/redesign.png';
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 
 export default function Projects() {
   return (
+
     <section className='projects-container'>
       <div className='projects-container_content'>
       
     <div className='projects-container_content_image'>
     <p>We design your <span>home.</span></p>
-        <img src={img}  alt="Project Image" />
+        <img src={redesign}  alt="Project" />
     </div>
     <button className="button left">
       <IoIosArrowBack  />
@@ -26,5 +27,6 @@ export default function Projects() {
     </div>
       </div>
     </section>
+    
   )
 }
